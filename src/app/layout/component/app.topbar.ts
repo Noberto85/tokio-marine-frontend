f8975c72de0454341743feda19b6ccb756c581fc
Noberto85 <nobertorafael9@gmail.com>
2025-05-11 14:13:40 -0300

    primeiro commit: limpando template primeng

---
+++ src/app/layout/component/app.topbar.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { StyleClassModule } from 'primeng/styleclass';
+import { AppConfigurator } from './app.configurator';
+import { LayoutService } from '../service/layout.service';
+
+@Component({
+    selector: 'app-topbar',
+    standalone: true,
+    imports: [RouterModule, CommonModule, StyleClassModule],
+    template: ` <div class="layout-topbar">
+        <div class="layout-topbar-logo-container">
+            <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
+                <i class="pi pi-bars"></i>
+            </button>
+           <span>SAKAI</span>
+        </div>
+    </div>`
+})
+export class AppTopbar {
+    items!: MenuItem[];
+
+    constructor(public layoutService: LayoutService) { }
+
+}
